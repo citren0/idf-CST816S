f977d695ea226c9cedc36868c3d31a755674a8f4
citren0 <cameron.davis1944@gmail.com>
2025-03-08 23:34:52 -0700

    CST working with interrupts and lvgl.

--- cst816s.c
+++ cst816s.c
@@ -5,20 +5,100 @@
 i2c_master_bus_handle_t bus_handle;
 i2c_master_dev_handle_t dev_handle;
 
+static QueueHandle_t events = NULL;
+static SemaphoreHandle_t semaphore = NULL;
 
-esp_err_t init_interrupt(int pin)
+static struct touch last_touch = { 0 };
+static uint8_t last_is_touched = 0;
+
+
+static void IRAM_ATTR isr_handler(void * args)
 {
-    // TODO
+    uint8_t item = 1;
+    xQueueSendFromISR(events, &item, NULL);
+}
+
+
+void get_touch(uint8_t * out_is_touched, struct touch * out_touch)
+{
+    if (xSemaphoreTake(semaphore, 0))
+    {
+        *out_is_touched = last_is_touched;
+        out_touch->x = last_touch.x;
+        out_touch->y = last_touch.y;
+        
+        xSemaphoreGive(semaphore);
+    }
+    // Don't overwrite anything if we can't get the semaphore.
+}
+
+
+void check_touch_task(void * args)
+{
+    uint8_t receive;
+
+    while (1)
+    {
+        if (xQueueReceive(events, &receive, 0))
+        {
+            if (xSemaphoreTake(semaphore, 5 / portTICK_PERIOD_MS))
+            {
+                if ((last_is_touched = is_touched()) == 1)
+                {
+                    last_touch.x = get_x_coordinate();
+                    last_touch.y = get_y_coordinate();
+                }
+
+                xSemaphoreGive(semaphore);
+            }
+        }
+
+        vTaskDelay(20 / portTICK_PERIOD_MS);
+    }
+}
+
+
+esp_err_t init_cst_interrupt(void)
+{
+    events = xQueueCreate(1, sizeof(char));
+    semaphore = xSemaphoreCreateMutex();
+
+    xTaskCreate(check_touch_task, "CST816S_check_touch_task", 4096, NULL, 5, NULL);
+
+    gpio_config_t int_config = {
+        .pin_bit_mask = (1ULL << CST_INT_GPIO),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_NEGEDGE,
+    };
+    gpio_config(&int_config);
+
+    gpio_install_isr_service(0);
+
+    gpio_isr_handler_add(CST_INT_GPIO, isr_handler, NULL);
+
     return ESP_OK;
 }
 
 
-esp_err_t init_cst(int scl, int sda)
+esp_err_t init_cst(void)
 {
+    // Reset chip.
+    gpio_set_direction(CST_RST_GPIO, GPIO_MODE_OUTPUT);
+
+    gpio_set_level(CST_RST_GPIO, 1);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+    gpio_set_level(CST_RST_GPIO, 0);
+    vTaskDelay(5 / portTICK_PERIOD_MS);
+    gpio_set_level(CST_RST_GPIO, 1);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+
+    // Init I2C
     i2c_master_bus_config_t bus_config = {
         .i2c_port = I2C_NUM_0,
-        .sda_io_num = sda,
-        .scl_io_num = scl,
+        .sda_io_num = CST_SDA_GPIO,
+        .scl_io_num = CST_SCL_GPIO,
         .clk_source = I2C_CLK_SRC_DEFAULT,
         .flags.enable_internal_pullup = false,
     };
@@ -45,7 +125,7 @@ uint16_t get_y_coordinate(void)
     ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, lower_buf, sizeof(lower_buf), &data[0], 1, CST_I2C_TIMEOUT / portTICK_PERIOD_MS));
     ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, upper_buf, sizeof(upper_buf), &data[1], 1, CST_I2C_TIMEOUT / portTICK_PERIOD_MS));
 
-    return ((data[1] << 8) | (data[0] << 0));
+    return (((data[1] & LOWER_4_BIT_MASK) << 8) | (data[0] << 0));
 }
 
 
@@ -59,5 +139,17 @@ uint16_t get_x_coordinate(void)
     ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, lower_buf, 1, &data[0], 1, CST_I2C_TIMEOUT / portTICK_PERIOD_MS));
     ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, upper_buf, 1, &data[1], 1, CST_I2C_TIMEOUT / portTICK_PERIOD_MS));
 
-    return ((data[1] << 8) | (data[0] << 0));
+    return (((data[1] & LOWER_4_BIT_MASK) << 8) | (data[0] << 0));
+}
+
+
+uint8_t is_touched(void)
+{
+    uint8_t data;
+
+    uint8_t cmd[1] = { CST_FINGER_NUM };
+
+    ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, cmd, 1, &data, 1, CST_I2C_TIMEOUT / portTICK_PERIOD_MS));
+
+    return data;
 }
--- include/cst816s.h
+++ include/cst816s.h
@@ -3,24 +3,44 @@
 #define CST816S_H_
 
 #include "esp_err.h"
+#include "esp_log.h"
 #include "driver/i2c_master.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
+#include "freertos/queue.h"
 
-#define CST_I2C_ADDR    0x15
-#define CST_I2C_FREQ    40000
-#define CST_I2C_TIMEOUT 1000
+// Pin definition.
+#define CST_SCL_GPIO        GPIO_NUM_10
+#define CST_SDA_GPIO        GPIO_NUM_11
+#define CST_RST_GPIO        GPIO_NUM_13
+#define CST_INT_GPIO        GPIO_NUM_14
 
-// Registers
-#define CST_LOWER_Y     ((uint8_t)0x06)
-#define CST_UPPER_Y     ((uint8_t)0x05)
-#define CST_LOWER_X     ((uint8_t)0x04)
-#define CST_UPPER_X     ((uint8_t)0x03)
+// I2C Definition.
+#define CST_I2C_ADDR        0x15
+#define CST_I2C_FREQ        40000
+#define CST_I2C_TIMEOUT     1000
 
-esp_err_t init_interrupt(int pin);
-esp_err_t init_cst(int scl, int sda);
+// Registers.
+#define CST_LOWER_Y         ((uint8_t)0x06)
+#define CST_UPPER_Y         ((uint8_t)0x05)
+#define CST_LOWER_X         ((uint8_t)0x04)
+#define CST_UPPER_X         ((uint8_t)0x03)
+#define CST_FINGER_NUM      ((uint8_t)0x02)
+
+#define LOWER_4_BIT_MASK    0x0f
+
+struct touch
+{
+    uint16_t x;
+    uint16_t y;
+};
+
+esp_err_t init_cst_interrupt(void);
+esp_err_t init_cst(void);
 uint16_t get_y_coordinate(void);
 uint16_t get_x_coordinate(void);
-
+void get_touch(uint8_t * out_is_touched, struct touch * out_touch);
+uint8_t is_touched(void);
 
 #endif // CST816S_H_
